4c5e2671a36668e357199d9ba499371a9894e750
Jake-Evans17 <49072138+Jake-Evans17@users.noreply.github.com>
2019-04-12 10:51:47 +0100

    Add files via upload

---
+++ recipe-app/target/classes/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO RECIPE (id, name, owner, description) VALUES (1, 'Food', 'Tim', 'qweqweq');
+INSERT INTO RECIPE (id, name, owner, description) VALUES (2, 'Soup', 'Jim', 'asdasdasd');
+INSERT INTO RECIPE (id, name, owner, description) VALUES (3, 'Pie', 'Pam', 'ertdfvsds');
+INSERT INTO RECIPE (id, name, owner, description) VALUES (4, 'Cake', 'Man', 'qwasdzxc');
